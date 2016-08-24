69bebdff79bee582908958212653cf280bb81be0
a-saitou <doredoko@gmail.com>
2016-08-24 22:47:54 +0900

    #

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Gmaps::Application.routes.draw do
-  get "map/index"
+
   resources :users
-  root 'maps#index'
+  root 'map#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
